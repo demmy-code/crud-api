f3ae30797e52dc34dd09d180c761f221356fe765
Demilade <dolaniyan32@gmail.com>
2026-06-18 12:54:54 +0100

    MY first commit

---
+++ routes/product.route.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const Product = require("../models/products.models.js");
+const router = express.Router();
+const {getProducts, getProduct, createProduct, updateProduct, deleteProduct} = require('../controller/product.controller.js');
+
+router.get('/', getProducts);
+router.get("/:id", getProduct);
+router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+
+
+
+module.exports = router;
